7959c84ed9a2d16e099f7737387c19d1bcea61a1
René Kriegler <post@rene-kriegler.com>
2026-05-28 21:16:47 +0200

    Update copyright year

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -50,7 +50,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'querqy.org'
-copyright = '2020-25, querqy.org documentation authors'
+copyright = '2020-26, querqy.org documentation authors'
 author = 'René Kriegler'
 
 # The version info for the project you're documenting, acts as replacement for
